b36da51fb9fc00c18f0c9457ab3ffdbe35209de7
Shivam <97872322+shivam-rtk@users.noreply.github.com>
2025-08-14 14:46:15 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,11 +1,13 @@
+#Load packages
 import pickle
 from flask import Flask, request, jsonify
 import pandas as pd
- 
+
 # Load the trained model
 with open('lr_model.bin', 'rb') as f_in:
     model = pickle.load(f_in)
     
+#Feature preparation
 def prepare_features(ride):
     return pd.DataFrame([{
         'cylinders': ride['cylinders'],
@@ -13,14 +15,17 @@ def prepare_features(ride):
         'weight': ride['weight'],
         'acceleration': ride['acceleration']
     }])
- 
+
+#Prediction
 def predict(features_df):
-    preds = model.predict(features_df)
+    preds = gbr.predict(features_df)
     return float(preds[0])
- 
-app = Flask('mpg-prediction')
+
+#API call
+app = Flask('MPG_Prediction')
  
 @app.route('/predict', methods=['POST'])
+
 def predict_endpoint():
     ride = request.get_json()
  
@@ -35,5 +40,3 @@ def predict_endpoint():
  
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=9696)
- 
- 
